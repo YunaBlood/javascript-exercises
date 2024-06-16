d56a8df9cb03663c89f11774729c083fee500677
Valentin <52529145+YunaBlood@users.noreply.github.com>
2024-06-16 19:29:17 +0200

    Added function to reverse string and passed all test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+//Initalize parameters to reverse and return input reversed
+const reverseString = function(string) {
+//Create a variable that can store original string
+//Create variable that store the result reversed
+let reversed = "";
+// Make a for loop to reverse the string
+// for(let i = string.length - 1; i >= 0; i--){
+//     reversed += string[i];
+// }
+
+//METHOD 2
+//Use split join and reverse function method
+reversed = string.split('').reverse().join('');
+
+return reversed;
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
