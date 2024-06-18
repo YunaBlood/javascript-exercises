852c6c57aff5a77280e34332f40441af1ea379d7
Valentin <52529145+YunaBlood@users.noreply.github.com>
2024-06-18 10:08:19 +0200

    Added two function that take the temperature in Fahrenheit and Celsius and convert it with decimal
    All test were passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp, celsiusTemp,celsiusDecimalTemp) {
+  celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+  celsiusDecimalTemp = Math.round(celsiusTemp * 10) / 10
+  return celsiusDecimalTemp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CelsiusTemp, FahrenheitTemp, FahrenheitDecimalTemp) {
+  FahrenheitTemp = (CelsiusTemp * 9/5) + 32
+  FahrenheitDecimalTemp = Math.round(FahrenheitTemp * 10) / 10
+  return FahrenheitDecimalTemp
 };
 
 // Do not edit below this line
