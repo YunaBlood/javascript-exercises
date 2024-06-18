d9f90ac61e89fd224b129ce7810301cacaffb4e3
Valentin <52529145+YunaBlood@users.noreply.github.com>
2024-06-18 10:04:41 +0200

    Added function that add all integer between two number and check if number and return ERROR
    Passed all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start, end, range=[]) {
+    if (start < 0  || end < 0) return "ERROR"
+    if (isNaN(start) || isNaN(end)) return "ERROR"
+    if (Number(start) !== start || Number(end) !==end) return "ERROR"
+    for(let i = Math.min(start,end); i <= Math.max(start,end); i++){
+        range.push(i);
+    }
+    return range.reduce((start,end) => start+end);
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
