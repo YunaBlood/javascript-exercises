e0423b3ffa2283a3138d8f083699f47f33d18183
Valentin <52529145+YunaBlood@users.noreply.github.com>
2024-10-09 11:05:41 +0200

    Pass all test with the function fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (countNumber) {
+  let count;
+  count = countNumber;
 
+  if (count < 0) return "OOPS";
+  if (count == 0) return 0;
+
+  let firstNumber = 1;
+  let secondNumber = 0;
+
+  for (let i = 2; i <= count; i++) {
+    let sum = firstNumber + secondNumber;
+    secondNumber = firstNumber;
+    firstNumber = sum;
+  }
+  return firstNumber;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
