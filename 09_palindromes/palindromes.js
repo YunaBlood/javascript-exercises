fa2c1a3e4308677323b04934de4ed3e2bf93b971
Valentin <52529145+YunaBlood@users.noreply.github.com>
2024-10-09 10:14:15 +0200

    Added function to check palindromes pass all test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const regex = /[^a-z0-9]/g;
+  const cleanStr = str.toLowerCase().replace(regex, "");
+  const reverseStr = cleanStr.split("").reverse().join("");
+  const isPalindrome = cleanStr === reverseStr;
+  return isPalindrome;
+  // if (str === str) {
+  //   return true;
+  // } else {
+  //   return false;
+  // } //Work but not for all test the one with return true and unevenly space number need to remove the char before comparison
 };
 
 // Do not edit below this line
