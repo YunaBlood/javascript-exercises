ce82403b3354df64ade1cd48da19ccd1ec7b6d24
Valentin <52529145+YunaBlood@users.noreply.github.com>
2024-10-09 10:17:55 +0200

    Added comment to explain more the logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,13 @@
 const palindromes = function (str) {
+  //variable that store all string and number on the bracket
   const regex = /[^a-z0-9]/g;
+  //Create a clean string which will transform it on lowerCase and replace the string that are in the regex by whitespace
   const cleanStr = str.toLowerCase().replace(regex, "");
+  //Store the cleanString and split it reverse it and join for comparison with the clean string
   const reverseStr = cleanStr.split("").reverse().join("");
+  //Compare the cleanStr with the reverseStr
   const isPalindrome = cleanStr === reverseStr;
+  //Return true or false
   return isPalindrome;
   // if (str === str) {
   //   return true;
